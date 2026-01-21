202555ee46cb4d4a290f24b7494a3817147a916b
Miroredrpo <avgweedy8@gmail.com>
2026-01-21 17:32:47 +0545

    FixSupabaseMigration

--- flask_sms/build.sh
+++ flask_sms/build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 # Initialize database and seed it
-python -c "from app import create_app, db; app = create_app('production'); app.app_context().push(); db.create_all()"
+# python -c "from app import create_app, db; app = create_app('production'); app.app_context().push(); db.create_all()"
 # Run seeder
-python seed.py
+# python seed.py
